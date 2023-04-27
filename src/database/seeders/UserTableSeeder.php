55f927aa7aa9d4e60cb8bc795218801c9d3e0205
simonaszz <simasak56@gmail.com>
2023-04-27 23:33:20 +0300

    seeder user table

--- src/database/seeders/UserTableSeeder.php
+++ src/database/seeders/UserTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserTableSeeder extends Seeder
 {
@@ -12,6 +14,40 @@ class UserTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        DB::table('users')->insert([
+            //Admin (0)
+
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('123'),
+                'role' => '0',
+                'status' => 'active',
+            ],
+            //Vendor(1)
+            [
+                'name' => 'Simonas Vendor',
+                'username' => 'Simonas',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('123'),
+                'role' => '1',
+                'status' => 'active',
+            ],
+            //User(2)
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('123'),
+                'role' => '2',
+                'status' => 'active',
+            ],
+
+
+        ]);
+
+
     }
 }
