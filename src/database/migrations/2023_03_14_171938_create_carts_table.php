2255671414b71210e9de16e04f303c1e71d3bfd5
simonaszz <simasak56@gmail.com>
2023-04-27 20:46:48 +0300

    project update

---
+++ src/database/migrations/2023_03_14_171938_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+
+            $table->foreignId('account_id')->constrained('accounts')->cascadeOnDelete();
+
+            $table->integer('amount')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
